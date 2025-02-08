5dbd723d2d59e4a906cc96ff30e1a422f358ec33
Ahmadalwakai76 <ahmadalwakai76@gmail.com>
2025-02-08 18:03:12 +0000

    Added base config to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Ai-mo-van-and-man/",  // ✅ هذا السطر مهم لجعل الموقع يعمل على GitHub Pages
-})
+import { defineConfig } from 'vite' 
+import react from '@vitejs/plugin-react' 
+ 
+export default defineConfig({ 
+  plugins: [react()], 
+  base: "/Ai-mo-van-and-man/",  // ??? ??? ????? 
+}) 
